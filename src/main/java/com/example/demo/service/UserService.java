26cfd3010669acd9565e319d718ecb112572d80a
Wagner Salbego <105342@urisantiago.br>
2025-05-20 12:43:14 -0300

    Melhoria no codigo deixando mais simples e remoção da HomeControler

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,38 +3,37 @@ package com.example.demo.service;
 import com.example.demo.model.User;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class UserService {
-    private final Map<Long, User> users = new ConcurrentHashMap<>();
+
+    private final Map<Long, User> userMap = new HashMap<>();
     private final AtomicLong idCounter = new AtomicLong();
 
     public List<User> getAllUsers() {
-        return new ArrayList<>(users.values());
+        return new ArrayList<>(userMap.values());
     }
 
     public User createUser(User user) {
         long id = idCounter.incrementAndGet();
         user.setId(id);
-        users.put(id, user);
+        userMap.put(id, user);
         return user;
     }
 
-    public User updateUser(Long id, User user) {
-        if (users.containsKey(id)) {
-            user.setId(id);
-            users.put(id, user);
-            return user;
+    public Optional<User> updateUser(Long id, User updatedUser) {
+        if (userMap.containsKey(id)) {
+            updatedUser.setId(id);
+            userMap.put(id, updatedUser);
+            return Optional.of(updatedUser);
+        } else {
+            return Optional.empty();
         }
-        return null;
     }
 
     public boolean deleteUser(Long id) {
-        return users.remove(id) != null;
+        return userMap.remove(id) != null;
     }
 }
